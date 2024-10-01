5577a874b14e7f1d6b4a72d8417d30342c3a5d4a
rkdeofuf <asa47972000@naver.com>
2024-10-01 22:13:24 +0900

    starbucks project - fix active badge box

--- FE/projects/starbucks/js/main.js
+++ FE/projects/starbucks/js/main.js
@@ -1,9 +1,63 @@
+// BadgeBox State
+let badgeRequestFrameId = null
+let isBadgeHidden = false
+let badgeOpacity = 1
+const REDUCING_OPACITY_RATIO = 0.85
+const INCREASING_OPACITY_RATIO = 1.15
+
 const badgeBoxEl = document.querySelector('.badge_box')
 const toTopButtonEl = document.querySelector('.to_top')
 
+const hiddenBadge = () => {
+    badgeOpacity *= REDUCING_OPACITY_RATIO
+    console.log(badgeOpacity)
+    if (badgeOpacity < 0.00000001) {
+        badgeOpacity = 0
+        badgeBoxEl.style.opacity = badgeOpacity
+        badgeBoxEl.style.display = 'none'
+        cancelAnimationFrame(badgeRequestFrameId)
+        badgeRequestFrameId = null
+        return
+    }
+    badgeBoxEl.style.opacity = badgeOpacity
+    badgeRequestFrameId = requestAnimationFrame(hiddenBadge)
+    return
+}
+
+const showBadge = () => {
+    badgeBoxEl.style.display = 'block'
+    if (badgeOpacity < 0.05) badgeOpacity = 0.05
+    badgeOpacity *= INCREASING_OPACITY_RATIO
+    if (badgeOpacity > 1) {
+        badgeOpacity = 1
+        badgeBoxEl.style.opacity = badgeOpacity
+        cancelAnimationFrame(badgeRequestFrameId)
+        badgeRequestFrameId = null
+        return
+    }
+    badgeBoxEl.style.opacity = badgeOpacity
+
+    badgeRequestFrameId = requestAnimationFrame(showBadge)
+
+    return
+}
+
 const activeBadgeBox = () => {
-    if (scrollY > 200) badgeBoxEl.classList.add('hidden')
-    else badgeBoxEl.classList.remove('hidden')
+    if (scrollY > 200 && isBadgeHidden === false) {
+        isBadgeHidden = true
+        if (badgeRequestFrameId !== null) {
+            cancelAnimationFrame(badgeRequestFrameId)
+            badgeRequestFrameId = null
+        }
+        badgeRequestFrameId = requestAnimationFrame(hiddenBadge)
+    } else if (scrollY < 200 && isBadgeHidden === true) {
+        isBadgeHidden = false
+        if (badgeRequestFrameId !== null) {
+            cancelAnimationFrame(badgeRequestFrameId)
+            badgeRequestFrameId = null
+        }
+        badgeRequestFrameId = requestAnimationFrame(showBadge)
+    }
 }
 
 const activeToTopButton = () => {
