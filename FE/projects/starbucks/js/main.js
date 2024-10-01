76ece8ca98bfb0aab1d60a82c64bdf130d140059
rkdeofuf <asa47972000@naver.com>
2024-10-01 21:19:01 +0900

    starbucks project - add active to_top btn

--- FE/projects/starbucks/js/main.js
+++ FE/projects/starbucks/js/main.js
@@ -1,16 +1,31 @@
 const badgeBoxEl = document.querySelector('.badge_box')
+const toTopButtonEl = document.querySelector('.to_top')
 
 const activeBadgeBox = () => {
     if (scrollY > 200) badgeBoxEl.classList.add('hidden')
     else badgeBoxEl.classList.remove('hidden')
 }
 
+const activeToTopButton = () => {
+    if (scrollY > 200) toTopButtonEl.classList.add('active')
+    else toTopButtonEl.classList.remove('active')
+}
+
+const scrollToZero = () => {
+    scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })
+}
+
 const onWindowScroll = (e) => {
     activeBadgeBox()
+    activeToTopButton()
 }
 
 const onWindowLoad = () => {
     window.addEventListener('scroll', onWindowScroll)
+    toTopButtonEl.addEventListener('click', scrollToZero)
 }
 
 window.addEventListener('load', onWindowLoad)
