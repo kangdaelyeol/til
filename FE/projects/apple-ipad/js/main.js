e228e811c3f851f1107ce854d277b6b3dae13846
rkdeofuf <asa47972000@naver.com>
2024-10-12 10:53:49 +0900

    add basket starter action

--- FE/projects/apple-ipad/js/main.js
+++ FE/projects/apple-ipad/js/main.js
@@ -1,1 +1,29 @@
-console.log('test')
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = document.querySelector('header .basket')
+
+// Header - basket action
+
+const showBasket = () => {
+    basketEl.classList.add('show')
+}
+
+const hideBasket = () => {
+    basketEl.classList.remove('show')
+}
+
+basketStarterEl.addEventListener('click', (e) => {
+    e.stopPropagation()
+    if (basketEl.classList.contains('show')) {
+        hideBasket()
+    } else {
+        showBasket()
+    }
+})
+
+basketEl.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+
+window.addEventListener('click', () => {
+    hideBasket()
+})
