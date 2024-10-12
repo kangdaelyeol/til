38634fb2be145a2edcc9be9a565d4d75f192a609
rkdeofuf <asa47972000@naver.com>
2024-10-12 23:27:21 +0900

    add searchbar action

--- FE/projects/apple-ipad/js/main.js
+++ FE/projects/apple-ipad/js/main.js
@@ -1,5 +1,11 @@
-const basketStarterEl = document.querySelector('header .basket-starter')
-const basketEl = document.querySelector('header .basket')
+const headerEl = document.querySelector('header')
+
+const basketStarterEl = headerEl.querySelector('.basket-starter')
+const basketEl = headerEl.querySelector('.basket')
+
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
 
 // Header - basket action
 
@@ -27,3 +33,23 @@ basketEl.addEventListener('click', (e) => {
 window.addEventListener('click', () => {
     hideBasket()
 })
+
+// Header - search bar action
+
+const showSearch = (e) => {
+    headerEl.classList.add('searching')
+    document.documentElement.classList.add('fixed')
+    e.stopPropagation()
+}
+
+const hideSearch = () => {
+    headerEl.classList.remove('searching')
+    document.documentElement.classList.remove('fixed')
+}
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchWrapEl.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+window.addEventListener('click', hideSearch)
