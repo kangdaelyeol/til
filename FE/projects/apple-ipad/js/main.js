60fe8d399f6bf48de109f1cabd6256e07450aa3b
rkdeofuf <asa47972000@naver.com>
2024-10-13 00:05:00 +0900

    add searchbar animation

--- FE/projects/apple-ipad/js/main.js
+++ FE/projects/apple-ipad/js/main.js
@@ -1,11 +1,25 @@
 const headerEl = document.querySelector('header')
 
+const headerMenuIconElList = [...headerEl.querySelectorAll('.menu li')]
+
 const basketStarterEl = headerEl.querySelector('.basket-starter')
 const basketEl = headerEl.querySelector('.basket')
 
 const searchWrapEl = headerEl.querySelector('.search-wrap')
 const searchStarterEl = headerEl.querySelector('.search-starter')
 const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+
+const searchInputEl = searchWrapEl.querySelector('input')
+const searchAutocompletesTitleEl = searchWrapEl.querySelector('h3')
+const searchAutocompletesItemElList = searchWrapEl.querySelectorAll(
+    '.autocompletes ul li'
+)
+
+const searchItemList = [
+    searchInputEl,
+    searchAutocompletesTitleEl,
+    ...searchAutocompletesItemElList,
+]
 
 // Header - basket action
 
@@ -39,12 +53,30 @@ window.addEventListener('click', () => {
 const showSearch = (e) => {
     headerEl.classList.add('searching')
     document.documentElement.classList.add('fixed')
+    headerMenuIconElList.reverse().forEach((el, index) => {
+        el.style.transitionDelay =
+            (0.4 * index) / headerMenuIconElList.length + 's'
+    })
+    searchItemList.forEach((el, index) => {
+        el.style.transitionDelay = (0.4 * index) / searchItemList.length + 's'
+    })
+
+    searchItemList.reverse()
     e.stopPropagation()
 }
 
 const hideSearch = () => {
     headerEl.classList.remove('searching')
     document.documentElement.classList.remove('fixed')
+    headerMenuIconElList.reverse().forEach((el, index) => {
+        el.style.transitionDelay =
+            (0.4 * index) / headerMenuIconElList.length + 's'
+    })
+    searchItemList.forEach((el, index) => {
+        el.style.transitionDelay = (0.4 * index) / searchItemList.length + 's'
+    })
+
+    searchItemList.reverse()
 }
 
 searchStarterEl.addEventListener('click', showSearch)
