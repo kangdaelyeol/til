12088e18d7c163bbd44a4e813421bd64bed027b6
rkdeofuf <asa47972000@naver.com>
2024-10-30 19:08:18 +0900

    add search component
    home route page에 포함되는 search Component 구조 및 스타일 구현

---
+++ FE/projects/movie-app/components/search.js
@@ -0,0 +1,13 @@
+import { Component } from '../src/core/core'
+
+export default class Search extends Component {
+    render() {
+        this.el.classList.add('search')
+        this.el.innerHTML = /* html */ `
+      <input placeholder="Enter the movie title to search!" />
+      <button class="btn btn-primary">
+        Search!
+      </button>
+    `
+    }
+}
--- FE/projects/movie-app/src/routes/home.js
+++ FE/projects/movie-app/src/routes/home.js
@@ -1,10 +1,12 @@
 import { Component } from '../core/core'
 import Headline from '../../components/headline'
-
+import Search from '../../components/search'
 export default class Home extends Component {
     render() {
         const headline = new Headline().el
-        headline.classList.add('container')
-        this.el.append(headline)
+        const search = new Search().el
+
+        this.el.classList.add('container')
+        this.el.append(headline, search)
     }
 }
