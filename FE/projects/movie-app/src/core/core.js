45c0b7dc166de54d4470d2a60dfc917bc38b3cc4
rkdeofuf <asa47972000@naver.com>
2024-10-30 17:41:27 +0900

    init project

---
+++ FE/projects/movie-app/src/core/core.js
@@ -0,0 +1,69 @@
+export class Component {
+    constructor(payload) {
+        const { tagName = 'div' } = payload
+        this.el = document.createElement(tagName)
+        render()
+    }
+
+    render() {}
+}
+
+function renderRouter(routes) {
+    if (!location.hash) {
+        history.replaceState(null, '', '/#/')
+    }
+
+    const routerView = document.querySelector('router-view')
+
+    const [hash, queryString = ''] = location.hash.split('?')
+
+    const query = queryString.split('&').reduce((acc, cur) => {
+        const [key, val] = cur.split('=')
+        acc[key] = val
+        return acc
+    }, {})
+
+    history.replaceState(query, '')
+
+    const currentRoute = routes.find((route) =>
+        new RegExp(`${route.path}\/?$`).test(hash),
+    )
+
+    routerView.innerHTML = ''
+    routerView.append(new currentRoute.component().el)
+
+    window.scrollTo(0, 0)
+}
+
+export function createRouter(routes) {
+    return function () {
+        window.addEventListener('popstate', () => {
+            renderRouter(routes)
+        })
+
+        renderRouter(routes)
+    }
+}
+
+export class Store {
+    constructor(state) {
+        this.state = {}
+        this.observers = {}
+
+        for (const key in state) {
+            Object.defineProperty(this.state, key, {
+                get: () => state[key],
+                set: (val) => {
+                    state[key] = val
+                    this.observers[key].forEach((observer) => observer(val))
+                },
+            })
+        }
+    }
+
+    subscribe(val, cb) {
+        Array.isArray(this.observers[val])
+            ? this.observers[val].push(cb)
+            : (this.observers[val] = [cb])
+    }
+}
