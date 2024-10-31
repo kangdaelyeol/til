175337c34cef3180b74c55505b51ccda4a9fa9b3
rkdeofuf <asa47972000@naver.com>
2024-10-31 18:04:32 +0900

    update store
    전체 영화 정보 개수에 대해 paging을 할 수 있도록 pageMax state 값을 추가

--- FE/projects/movie-app/src/store/movie.js
+++ FE/projects/movie-app/src/store/movie.js
@@ -4,21 +4,25 @@ const store = new Store({
     searchText: '',
     page: 1,
     movies: [],
+    pageMax: 1,
 })
 
 export default store
 
 export const searchMovies = async (page) => {
     if (page === 1) {
-        store.state.page = 1
         store.state.movies = []
     }
 
-    const { Search } = await (
+    const { Search, totalResults } = await (
         await fetch(
             `https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`,
         )
     ).json()
+
+    store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+
+    store.state.page = page
 
     store.state.movies = [...store.state.movies, ...Search]
 }
