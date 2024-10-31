c88a21d0b0e3821457420dc392d09a5e82c8a6e1
rkdeofuf <asa47972000@naver.com>
2024-10-31 19:01:01 +0900

    add annotation in movieListMore Component
    해당 Component의 Render 부분에 eventListener를 추가하는 부분이 있기 때문에 state가 변경됨으로써 호출되는 observer메서드에 render 메서드가 포함될시 중복으로 eventListener가 등록되어 paging기법을 observer부분에서 비교함을 명시.

--- FE/projects/movie-app/src/components/movieListMore.js
+++ FE/projects/movie-app/src/components/movieListMore.js
@@ -9,6 +9,8 @@ export default class MovieListMore extends Component {
         })
 
         movieStore.subscribe('page', () => {
+            // render 부분에 추가된 eventListener에 대한 clear function 부분이 없기 때문에
+            // this.render() 후 render 부분에 hide 클래스를 컨트롤 하는 기능을 부여하면 subscribe의 render호출로 인한 중복 eventListener 등록이 되어버림.
             const { pageMax, page } = movieStore.state
             pageMax <= page
                 ? this.el.classList.add('hide')
