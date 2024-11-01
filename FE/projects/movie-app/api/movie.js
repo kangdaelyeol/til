3899e36c2fd7059a27d1b876a046228cc7a2a21a
rkdeofuf <asa47972000@naver.com>
2024-11-01 20:33:53 +0900

    add dot env

--- FE/projects/movie-app/api/movie.js
+++ FE/projects/movie-app/api/movie.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
     const { title, page, id } = JSON.parse(req.body)
+    const { API_KEY } = process.env
     const url = id
-        ? `https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full`
-        : `https://omdbapi.com?apikey=7035c60c&s=${title}&page=${page}`
+        ? `https://omdbapi.com?apikey=${API_KEY}&i=${id}&plot=full`
+        : `https://omdbapi.com?apikey=${API_KEY}&s=${title}&page=${page}`
 
     res.status(200).json(await (await fetch(url)).json())
 }
