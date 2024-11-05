319b489df13d6aa25152a9220db63383505e81df
rkdeofuf <asa47972000@naver.com>
2024-11-05 20:50:17 +0900

    add interface-function

---
+++ FE/projects/ts-test/src/6.interface-function.ts
@@ -0,0 +1,26 @@
+// Interface - Function type (호출 시그니처: Call Signature)
+
+// call signature as interface
+interface GetName {
+    (param: string): string
+}
+
+// call signature as type alias
+type GetName2 = (param: string) => string
+
+interface TUser {
+    readonly name: string
+    age: number
+    isValid?: boolean
+    getName: GetName
+    getName2?: GetName2
+}
+
+const tUser: TUser = {
+    name: 'rkdeofuf',
+    age: 27,
+    getName(message: string) {
+        console.log(message)
+        return this.name
+    },
+}
