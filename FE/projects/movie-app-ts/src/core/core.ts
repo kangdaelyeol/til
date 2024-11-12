7e84acd7622ef2e6043afdc721ded2ea2d181832
rkdeofuf <asa47972000@naver.com>
2024-11-12 19:44:30 +0900

    typing - store

--- FE/projects/movie-app-ts/src/core/core.ts
+++ FE/projects/movie-app-ts/src/core/core.ts
@@ -65,7 +65,7 @@ function renderRouter(routes: Routes) {
     window.scrollTo(0, 0)
 }
 
-export function createRouter(routes) {
+export function createRouter(routes: Routes) {
     return function () {
         window.addEventListener('popstate', () => {
             renderRouter(routes)
@@ -75,11 +75,24 @@ export function createRouter(routes) {
     }
 }
 
-export class Store {
-    constructor(state) {
-        this.state = {}
-        this.observers = {}
+interface StoreObservers {
+    [ket: string]: SubscribeCallback[]
+}
 
+interface SubscribeCallback {
+    // call signature
+    (arg: unknown): void
+}
+
+// Store에서 사용되는 State 상태 객체의 프로퍼티 타입은 고정되어 있지 않다 -> Generic 사용
+export class Store<S> {
+    // state는 외부 컴포넌트에서 참조되어 사용 되므로 public access modifier 사용
+    // state 상태 객체의 기본 값은 빈 객체 초기화 이후 제네릭 타입의 객체 타입이 될 것이므로 제네릭 type assertion 해주기
+    public state = {} as S
+    // observers 객체는 클래스 인스턴스 내에서만 참조 되므로 private access modifier 사용
+    private observers = {} as StoreObservers
+
+    constructor(state: S) {
         for (const key in state) {
             Object.defineProperty(this.state, key, {
                 get: () => state[key],
@@ -93,7 +106,7 @@ export class Store {
         }
     }
 
-    subscribe(val, cb) {
+    subscribe(val: string, cb: SubscribeCallback) {
         Array.isArray(this.observers[val])
             ? this.observers[val].push(cb)
             : (this.observers[val] = [cb])
