8c0c1a1b19d52ae73cc87faff8834b0111ae967f
rkdeofuf <asa47972000@naver.com>
2024-11-12 20:42:15 +0900

    typing  - theHeader component

--- FE/projects/movie-app-ts/src/components/theHeader.ts
+++ FE/projects/movie-app-ts/src/components/theHeader.ts
@@ -1,7 +1,16 @@
 import { Component } from '../core/core'
 import aboutStore from '../store/about'
-
+interface State {
+    [key: string]: unknown
+    menuList: {
+        name: string
+        href: string
+    }[]
+}
 export default class TheHeader extends Component {
+    // 명확한 할당 단언 (!) - 초기화가 확실한 경우 allocation assertion을 사용할 수 있음 이는 ts에 값이 할당되어 있음을 알린다.
+    // 부모 클래스의 프로퍼티로써 값을 사용해야 하는 경우 파생 클래스의 프로퍼티를 초기화 하면 부모 클래스의 프로퍼티가 재정의(override) 되어 사용할 수 없다.
+    public state!: State
     constructor() {
         super({
             tagName: 'header',
