46e3c63e0011c393e0f15ed68d448066c2aa54b8
rkdeofuf <asa47972000@naver.com>
2024-11-18 19:30:30 +0900

    update type assertion

--- FE/projects/ts-test/src/3.type-assertion.ts
+++ FE/projects/ts-test/src/3.type-assertion.ts
@@ -4,11 +4,19 @@
 // Assertion Keyword - as
 // Non-null assertion operator - !
 
+/*
+ type 단언(assertion)은 코드상 정확한 타입 추론이 어려운 경우 개발자의 직접적인 개입을 통한 타입 확정 개념이다.
+ 코드 상으로 타입 에러를 개발자가 단언함으로써 없애기 때문에 타입 안전성을 해친다.
+ 주로 as 키워드를 사용해 단언하고, null, undefined 값이 아님을 확정 짓기 위해 `non-null` 연산자로써 `!` 기호를 사용한다.
+
+ 객체 리터럴의 경우 satisfies 키워드를 이용해 타입 단언 전에 타입 체크를 함으로써 안정성 있는 타입 단언을 할 수도 있다.
+*/
+
 // as 명시
-const el = document.querySelector('body') as HTMLBodyElement
+const el = document.querySelector('body') as HTMLBodyElement | null
 el.textContent = 'hellow'
 
-// ! 명시
+// ! (non-null operator) 명시
 const el2 = document.querySelector('body')
 el!.textContent = 'hello!'
 
@@ -38,3 +46,15 @@ getValue('abc', false)
 
 let numb!: number // ! 키워드 - 할당 했다고 단언 함으로써 nullable 에러 제거하기.
 console.log(numb)
+
+// satisfies keyword - object literal에 대한 타입 체크
+
+interface PersonI {
+    name: string
+    age: number
+}
+
+const p1: PersonI = {
+    name: 'rkdeofuf',
+    age: 27,
+} satisfies PersonI as PersonI // 객체 리터럴에 대한 타입 검사 (satisfies)
