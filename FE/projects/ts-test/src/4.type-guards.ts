ebbb65d68b75213823ae17eae9972cdbd55d473b
rkdeofuf <asa47972000@naver.com>
2024-11-18 19:17:16 +0900

    update type guard

--- FE/projects/ts-test/src/4.type-guards.ts
+++ FE/projects/ts-test/src/4.type-guards.ts
@@ -1,5 +1,8 @@
 // Type Guards - 타입 가드
-// 에러가 발생할 수 있는 상황을 코드 상에서 방어하는 기법
+/* 
+  type guard 는 JS 런타임 환경에서 변수의 잘못된 타입 참조로 인한 에러를 방지하기 위한 기법이다. 이를 통해 타입 안정성을 확보 할 수 있다.
+  ts 환경에서 typeof, instanceof, in 연산자로 직접 데이터 검사를 하거나 is 키워드를 사용하는 별도의 타입 체크 함수를 구현할 수 있다.
+*/
 
 function logText(el: Element) {
     console.log(el.textContent)
@@ -24,4 +27,9 @@ function logAdd(val: string | number | boolean | null) {
     }
 
     console.log(res)
+}
+
+// is keyword - type guard syntax
+function isHTMLElement(el: Element | null): el is HTMLElement {
+    return el instanceof HTMLElement
 }
