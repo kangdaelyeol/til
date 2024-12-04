5e953f342257083baf75c3196ec46ce4614680bd
rkdeofuf <asa47972000@naver.com>
2024-12-04 22:29:21 +0900

    update component - apply redux store
    note를 생성, 수정, 삭제할 수 있게 redux store 적용

--- FE/projects/react-note/src/components/NoteDetail.jsx
+++ FE/projects/react-note/src/components/NoteDetail.jsx
@@ -1,4 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams, useNavigate } from 'react-router-dom'
+import { deleteNote, updateNote } from '../store/notesSlice'
 const NoteDetail = () => {
+    const navigate = useNavigate()
+    const params = useParams()
+    const note = useSelector((state) =>
+        state.notes.find((note) => note.id === params.id),
+    )
+
+    const dispatch = useDispatch()
+
+    const handleChangeTitle = (e) => {
+        dispatch(
+            updateNote({
+                ...note,
+                title: e.target.value,
+            }),
+        )
+    }
+
+    const handleChangeContent = (e) => {
+        dispatch(
+            updateNote({
+                ...note,
+                content: e.target.value,
+            }),
+        )
+    }
+
+    const handleDelete = () => {
+        dispatch(deleteNote(params.id))
+        navigate('/')
+    }
+
     return (
         <div className="bg-gray-900 p-6">
             <div className="flex justify-between items-center mb-4">
@@ -6,11 +40,16 @@ const NoteDetail = () => {
                     <time className="text-gray-400 text-sm">2024</time>
                     <input
                         type="text"
+                        value={note.title}
+                        onChange={handleChangeTitle}
                         className="bg-transparent text-2xl font-bold focus-within:outline-blue-100"
                     />
                 </div>
                 <div>
-                    <button className="bg-red-600 hover:bg-red-500 py-2 px-4 rounded">
+                    <button
+                        onClick={handleDelete}
+                        className="bg-red-600 hover:bg-red-500 py-2 px-4 rounded"
+                    >
                         삭제
                     </button>
                 </div>
@@ -22,6 +61,8 @@ const NoteDetail = () => {
                         className="bg-gray-700 w-full h-64 p-2 rounded outline-none resize-none focus:(ring-2 ring-blue-500)"
                         name=""
                         id=""
+                        value={note.content}
+                        onChange={handleChangeContent}
                     ></textarea>
                     <button className="bg-blue-600 hover:bg-blue-500 py-2 px-4 mt-4 rounded">
                         요약
@@ -30,7 +71,7 @@ const NoteDetail = () => {
                 <div className="flex-1 p-4 rounded bg-gray-800">
                     <h3 className="text-lg font-semibold mb-2">요약 결과</h3>
                     <div className="text-gray-300 h-64 bg-gray-700 p-2 rounded">
-                        요약 내용
+                        {note.summary}
                     </div>
                 </div>
             </section>
--- FE/projects/react-note/src/components/SideBar.jsx
+++ FE/projects/react-note/src/components/SideBar.jsx
@@ -1,12 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-
+import { v4 as uuidv4 } from 'uuid'
+import { addNote } from '../store/notesSlice.js'
 const SideBar = () => {
     const navigate = useNavigate()
+
+    const notes = useSelector((state) => state.notes)
+    const dispatch = useDispatch()
+
     const handleClick = () => {
-        const id = 1
+        const id = uuidv4()
+        const newNote = {
+            id,
+            title: '새로운 노트',
+            content: '',
+            time: Date.now(),
+            summary: '',
+        }
+        dispatch(addNote(newNote))
         navigate(`/notes/${id}`)
     }
+
     return (
         <div className="w-[230px] p-4">
             <h1 className="text-2xl font-bold mb-4">FastCampus Note</h1>
@@ -27,6 +42,22 @@ const SideBar = () => {
                 >
                     홈
                 </NavLink>
+            </div>
+            <div className="mt-4">
+                {notes.map((note) => (
+                    <li key={note.id}>
+                        <NavLink
+                            className={({ isActive }) =>
+                                isActive
+                                    ? 'text-blue-500 text-semibold'
+                                    : 'text-gray-300 text-semibold hover:text-white'
+                            }
+                            to={`/notes/${note.id}`}
+                        >
+                            {note.title}
+                        </NavLink>
+                    </li>
+                ))}
             </div>
         </div>
     )
