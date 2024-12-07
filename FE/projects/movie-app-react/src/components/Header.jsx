7ca4d56996716622989ef1815b8ffb900556b98e
rkdeofuf <asa47972000@naver.com>
2024-12-07 15:27:12 +0900

    fix typo

--- FE/projects/movie-app-react/src/components/Header.jsx
+++ FE/projects/movie-app-react/src/components/Header.jsx
@@ -53,7 +53,11 @@ export default function Header() {
                 to="/"
                 className="w-[40px] h-[40px] rounded-[50%] bg-color-area cursor-pointer absolute top-0 bottom-0 right-[40px] m-auto transition-[0.3s] hover:scale-[1.2]"
             >
-                <img className="w-full" src="" alt="User" />
+                <img
+                    className="w-full"
+                    src="https://avatars.githubusercontent.com/u/27201345?v=4"
+                    alt="User"
+                />
             </NavLink>
         </div>
     )
--- FE/projects/movie-app-react/tailwind.config.js
+++ FE/projects/movie-app-react/tailwind.config.js
@@ -10,13 +10,12 @@ export default {
                 'color-white-30': 'rgba(255, 255, 255, 0.3)',
                 'color-white-20': 'rgba(255, 255, 255, 0.2)',
                 'color-white-10': 'rgba(255, 255, 255, 0.1)',
-                'color-white_5': 'rgba(255, 255, 255, 0.05)',
+                'color-white-5': 'rgba(255, 255, 255, 0.05)',
 
                 'color-primary': '#fdc000',
                 'color-hover': '#86a05',
-                'color-ared': '#1c212e',
+                'color-area': '#1c212e',
             },
-            
         },
     },
     plugins: [],
