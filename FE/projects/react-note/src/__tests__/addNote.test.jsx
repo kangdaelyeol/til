016db3fb874f046a097bc0a37b397fe4bcc33913
rkdeofuf <asa47972000@naver.com>
2024-12-07 12:49:41 +0900

    update addNote test
    노트를 추가하는 테스트 코드 작성,
    OpenAI를 mocking 하려 했지만 실패새 OpenAI 기능을 포함해 추상화한 fetchAPI를 mocking해서 테스트함.

--- FE/projects/react-note/src/__tests__/addNote.test.jsx
+++ FE/projects/react-note/src/__tests__/addNote.test.jsx
@@ -1,10 +1,25 @@
 import { createMemoryRouter, RouterProvider } from 'react-router-dom'
-import { describe } from 'vitest'
+import { describe, expect, vi } from 'vitest'
 import { routerConfig } from '../App'
 import { render, waitFor } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import store from '../store'
 import userEvent from '@testing-library/user-event'
+import { fetchOpenAI } from '../api'
+
+vi.mock('../api', () => ({
+    fetchOpenAI: vi.fn(),
+}))
+
+// vi.mock('openai', () => ({
+//     default: class {
+//         chat = {
+//             completions: {
+//                 create: vi.fn(),
+//             },
+//         }
+//     },
+// }))
 
 describe('노트 추가 및 편집 기능', () => {
     it('새로운 노트 추가, notes 라우팅, 사이드바에 링크 추가', async () => {
@@ -12,7 +27,7 @@ describe('노트 추가 및 편집 기능', () => {
         // memoryRouter - 내부 메모리상으로 간편하게 url routing 구현
         const router = createMemoryRouter(routerConfig)
 
-        const { getByText } = render(
+        const { getByText, getByTestId } = render(
             <Provider store={store}>
                 <RouterProvider router={router} />
             </Provider>,
@@ -27,5 +42,23 @@ describe('노트 추가 및 편집 기능', () => {
         })
 
         expect(getByText('새로운 노트')).toHaveAttribute('href', `/notes/${id}`)
+
+        const titleEl = getByTestId('title')
+        const contentEl = getByTestId('content')
+
+        await userEvent.clear(titleEl)
+        await userEvent.clear(contentEl)
+        await userEvent.type(titleEl, 'new title')
+        await userEvent.type(contentEl, 'new content')
+
+        const updatedNote = store.getState().notes.find((n) => n.id === id)
+        expect(updatedNote.title).toBe('new title')
+        expect(updatedNote.content).toBe('new content')
+
+        fetchOpenAI.mockResolvedValueOnce('요약된 내용')
+
+        await userEvent.click(getByText('요약'))
+        const updatedNote1 = store.getState().notes.find((n) => n.id === id)
+        expect(updatedNote1.summary).toBe('요약된 내용')
     })
 })
