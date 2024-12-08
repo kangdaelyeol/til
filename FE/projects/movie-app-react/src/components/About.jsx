225bad88c8257d28f8070825aa9487d4b6d06738
rkdeofuf <asa47972000@naver.com>
2024-12-08 10:27:24 +0900

    add About component
    tailwindcss의 bg-[url()] 속성이 적용 안됨. 따라서 element inline style 속성에 직접 추가

---
+++ FE/projects/movie-app-react/src/components/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const about = {
+    photo: 'https://avatars.githubusercontent.com/u/27201345?v=4',
+    name: 'DANIEL / KangDaeLyeol',
+    email: 'kdy0510123@gmail.com',
+    blog: 'https://heropy.blog',
+    github: 'https://github.com/kangdaelyeol',
+    repository: 'https://github.com/kangdaelyeol/kangdaelyeol.github.io',
+}
+
+export default function About() {
+    const { name, email, github, blog, photo } = about
+    return (
+        <div>
+            <div
+                // TailWindcss - bg-[url()] 적용 안됨.
+                className={`w-[230px] h-[230px] mx-auto mt-0 mb-[20px] rounded-[10px] bg-cover bg-[url(${photo})]`}
+                style={{
+                    backgroundImage: `url(${photo})`,
+                }}
+            ></div>
+            <p className="text-[40px] font-Oswald leading-[1.5] text-center mb-[4px]">
+                {name}
+            </p>
+            <p className="leading-[1.5] text-center mb-[4px]">
+                <Link
+                    className="text-color-primary hover:decoration-underline"
+                    to={`https://mail.google.com/main/?view=cm&fs=1&to=${email}`}
+                    target="_blank"
+                >
+                    {email}
+                </Link>
+            </p>
+            <p className="leading-[1.5] text-center mb-[4px]">
+                <Link
+                    className="text-color-primary hover:decoration-underline"
+                    to={github}
+                    target="_blank"
+                >
+                    GitHub
+                </Link>
+            </p>
+            <p className="leading-[1.5] text-center mb-[4px]">
+                <Link
+                    className="text-color-primary hover:decoration-underline"
+                    to={blog}
+                    target="_blank"
+                >
+                    Blog
+                </Link>
+            </p>
+        </div>
+    )
+}
