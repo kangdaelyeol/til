1d57c26d1d9aad78ef8a5c5e26e81db12cc478a6
rkdeofuf <asa47972000@naver.com>
2025-05-05 16:00:02 +0900

    add checkbox, todoinput component

--- FE/projects/react-ts-todo/src/App.tsx
+++ FE/projects/react-ts-todo/src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Checkbox from './components/checkbox';
+import TodoInput from './components/todo-input';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Todo {
+	id: number;
+	text: string;
+	done: boolean;
 }
 
-export default App
+function App() {
+	console.log('App rendered');
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const addTodo = (text: string) => {
+		setTodos((prev) => {
+			return [
+				...prev,
+				{
+					id: todos.length + 1,
+					text,
+					done: false,
+				},
+			];
+		});
+	};
+
+	const toggleTodo = (id: number) => {
+		setTodos((prev) => {
+			const newTodo = prev.map((todo) => {
+				if (todo.id === id) return { ...todo, done: !todo.done };
+
+				return todo;
+			});
+			return newTodo;
+		});
+	};
+
+	return (
+		<div className=''>
+			<TodoInput onAddTodo={addTodo} />
+			<ul>
+				{todos.map((todo) => {
+					return (
+						<li key={todo.id}>
+							<Checkbox
+								id={`todo-${todo.id}`}
+								checked={todo.done}
+								label={todo.text}
+								onChange={() => toggleTodo(todo.id)}
+							/>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
+}
+
+export default App;
---
+++ FE/projects/react-ts-todo/src/components/todo-input.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent, useState } from 'react';
+
+interface TodoInputProps {
+	onAddTodo: (text: string) => void;
+}
+
+export default function TodoInput({ onAddTodo }: TodoInputProps) {
+	const [newTodo, setNewTodo] = useState<string>('');
+
+	const addTodo = () => {
+		onAddTodo(newTodo);
+		setNewTodo('');
+	};
+
+	const onInputTodo = (e: ChangeEvent<HTMLInputElement>) => {
+		setNewTodo(e.currentTarget.value);
+	};
+
+	return (
+		<div>
+			<input type='text' value={newTodo} onChange={onInputTodo} />
+			<button onClick={addTodo}>추가</button>
+		</div>
+	);
+}
