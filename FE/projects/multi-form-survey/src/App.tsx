af8210b50031caa517cffe5e9db46d704c42a7ae
rkdeofuf <asa47972000@naver.com>
2025-05-09 19:05:39 +0900

    Feat: add react-router

--- FE/projects/multi-form-survey/src/App.tsx
+++ FE/projects/multi-form-survey/src/App.tsx
@@ -1,32 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import MainLayout from './components/common/main-layout'
-import Tabs, {
-    Tab,
-    TabList,
-    TabPanel,
-    TabPanels,
-} from './components/common/tabs'
 import SectionEditorList from './components/edit/section-editor-list'
 import { SurveyStoreProvider } from './store'
+import AdminPage from './pages/admin-page'
 
 function App() {
     return (
-        <MainLayout>
-            <SurveyStoreProvider>
-                <Tabs>
-                    <TabList>
-                        <Tab index={0}>tab1</Tab>
-                        <Tab index={1}>tab2</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel index={0}>
-                            <SectionEditorList />
-                        </TabPanel>
-                        <TabPanel index={1}>panel2</TabPanel>
-                    </TabPanels>
-                </Tabs>
-            </SurveyStoreProvider>
-        </MainLayout>
+        <BrowserRouter>
+            <MainLayout>
+                <SurveyStoreProvider>
+                    <Routes>
+                        <Route
+                            path="/surveys/:surveyId"
+                            element={<AdminPage />}
+                        >
+                            <Route
+                                path="edit"
+                                element={<SectionEditorList />}
+                            />
+                            <Route path="responses" element={<div>응답</div>} />
+                        </Route>
+                    </Routes>
+                </SurveyStoreProvider>
+            </MainLayout>
+        </BrowserRouter>
     )
 }
 
